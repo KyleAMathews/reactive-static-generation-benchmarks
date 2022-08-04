2bd44b437cbdf3435d9d8af6bbe8678ec165df46
Kyle Mathews <mathews.kyle@gmail.com>
2022-08-04 15:11:43 -0700

    add nextjs ssr manual cache-invalidation example

---
+++ ecommerce-nextjs-cache-invalidation/pages/api/revalidate.ts
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate(`/products/${req.body.productId}`)
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send(`Error revalidating`)
+  }
+}
---
+++ ecommerce-nextjs-cache-invalidation/pages/products/[id].tsx
@@ -0,0 +1,44 @@
+import got from "got"
+
+export async function getStaticPaths() {
+  const url = `https://reactive-static-generation-benchmarks-kyleamathews.chiselstrike.io/main/product\?sort\=productId`
+  const res = await got(url, { responseType: `json` })
+  const body = res.body
+  const paths = body.results.map((product) => {
+    return {
+      params: {
+        id: product.productId.toString(),
+      },
+    }
+  })
+  return {
+    paths: paths,
+    fallback: false, // can also be true or 'blocking'
+  }
+}
+
+// `getStaticPaths` requires using `getStaticProps`
+export async function getStaticProps(context) {
+  console.log(context)
+  const url = `https://reactive-static-generation-benchmarks-kyleamathews.chiselstrike.io/main/product?.productId=${context.params.id}`
+  console.log({ url })
+  const res = await got(url, { responseType: `json` })
+  const body = res.body
+  return {
+    // Passed to the page component as props
+    props: { product: body.results[0] },
+  }
+}
+
+export default function Product(props) {
+  console.log(props)
+  return (
+    <div>
+      <h1>{props.product.productId}</h1>
+      <p>
+        Inventory is:{` `}
+        <span id="inventory-level">{props.product.inventoryLevel}</span>
+      </p>
+    </div>
+  )
+}
