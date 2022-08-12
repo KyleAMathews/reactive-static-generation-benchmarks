9fcda7d431d30f81f7f3c368847b96c8a3cdb0a1
Kyle Mathews <mathews.kyle@gmail.com>
2022-08-12 12:36:29 -0700

    prettier

--- gatsby-ecommerce/src/pages/products/{Product.id}.tsx
+++ gatsby-ecommerce/src/pages/products/{Product.id}.tsx
@@ -2,10 +2,15 @@ import * as React from "react"
 import { graphql } from "gatsby"
 
 export default function Product(props) {
-  return <div>
-  <h1>{props.data.product.id}</h1>
-  <p>Inventory is: <span id="inventory-level">{props.data.product.inventoryLevel}</span></p>
-  </div>
+  return (
+    <div>
+      <h1>{props.data.product.id}</h1>
+      <p>
+        Inventory is:{" "}
+        <span id="inventory-level">{props.data.product.inventoryLevel}</span>
+      </p>
+    </div>
+  )
 }
 
 export const query = graphql`
